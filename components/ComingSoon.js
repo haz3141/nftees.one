762fb9095654d6cdd4ef6aaaab0bc2db57010004
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-18 21:43:12 -0400

    Moved component logic to seperate component

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -1,31 +1,8 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
+import useRandom from './useRandom';
 
 const comingSoon = () => {
-    useEffect(() => {
-        // document.querySelectorAll('.grid-background > div')
-        // Loop over grid items and create random duration and delay
-
-        document.querySelectorAll('.grid-background > div')
-            .forEach((elem) => {
-                // Calculate random number for delay
-                let delay = getRandomInt(0, 5);
-                // Calculate random number for duration
-                let duration = getRandomInt(3, 6);
-            
-                // Set both
-                elem.style.animationDelay = `${delay}s`;
-                elem.style.animationDuration = `${duration}s`;
-            });
-        
-        function getRandomInt(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-        
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    });
-
+    useRandom();
     return (
         <div className="relative min-h-screen flex items-center justify-center bg-gray-900 overflow-hidden">
 

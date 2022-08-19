a23f05177a557836fc86142db99a6b31be837cbe
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-19 17:41:34 -0400

    Configured Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };

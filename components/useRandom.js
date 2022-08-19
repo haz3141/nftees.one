75ff9f6a17eaa8d71fd6c03ee4c0821ccbf8fdf9
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-18 22:09:21 -0400

    Modularized ComingSoon.js

--- components/useRandom.js
+++ components/useRandom.js
@@ -1,28 +1,6 @@
-import { useEffect } from "react";
 
 const useRandom = () => {
-    useEffect(() => {
-        // document.querySelectorAll('.grid-background > div')
-        // Loop over grid items and create random duration and delay
-        document.querySelectorAll('.grid-background > div')
-            .forEach((elem) => {
-                // Calculate random number for delay
-                let delay = getRandomInt(0, 5);
-                // Calculate random number for duration
-                let duration = getRandomInt(3, 6);
-            
-                // Set both
-                elem.style.animationDelay = `${delay}s`;
-                elem.style.animationDuration = `${duration}s`;
-            });
-        
-        function getRandomInt(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-        
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    });
+    
 }
 
 export default useRandom;

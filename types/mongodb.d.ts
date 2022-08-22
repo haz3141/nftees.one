52919c3179723b286b291dec5d0d172e3065e927
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-22 13:53:22 -0400

    Removed mongodb files

--- types/mongodb.d.ts
+++
@@ -1,9 +0,0 @@
-/* Diable ESLint for no use of var */
-/* as here it is needed for scope */
-/* Only applies to dev environment */
-/* eslint-disable no-var */
-import {MongoClient} from 'mongodb';
-
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
-}

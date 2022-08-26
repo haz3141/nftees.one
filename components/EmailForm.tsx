b3c993c336d0808118232d0b34ff9acbb8339768
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-26 16:03:07 -0400

    Styled form

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -30,9 +30,9 @@ const EmailForm = () => {
     <div className="block m-6 p-6 rounded-lg shadow-lg bg-white">
       <h2
         className="text-center tracking-wider
-        text-sm md:text-2xl mb-3"
+        text-black text-sm md:text-2xl mb-3"
       >
-        Subscribe to get the latest
+        Subscribe to get the latest{' '}
         <span className="text-orange-400">news & updates</span>.
       </h2>
       <form method="POST" onSubmit={handleSubmit}>
